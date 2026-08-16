30c3e02ec9794692c9973d9cd915fdaf578e35bc
Eyob <eyobk740@gmail.com>
2026-08-16 22:35:04 +0300

    Add help and exit commands with registry patterns

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
+    commands := getCommands()
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
-		fmt.Print("Pokedex >")
+		fmt.Print("Pokedex > ")
 		if !scanner.Scan() {
 			if err := scanner.Err(); err != nil {
 				fmt.Fprintln(os.Stderr, "Error reading input:", err)
@@ -21,6 +23,16 @@ func main() {
 		if len(cleanedInput) == 0 {
 			continue
 		}
-		fmt.Print("Your command was: ", cleanedInput[0], "\n")
+
+		commandName := cleanedInput[0]
+		command, exists := commands[commandName]
+		if !exists{
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil{
+			fmt.Println("Err:", err)
+		}
 	}
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,52 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
+
+func commandHelp() error{
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := getCommands()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	return nil
+}
+
+
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
