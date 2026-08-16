ac1cf0f75971bdf649606db7f264cc447d8ced3d
Eyob <eyobk740@gmail.com>
2026-08-16 19:43:15 +0300

    Add REPL loop with input cleaning

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			}
+			return
+		}
+		line := scanner.Text()
+		cleanedInput := cleanInput(line)
+		if len(cleanedInput) == 0 {
+			continue
+		}
+		fmt.Print("Your command was: ", cleanedInput[0], "\n")
+	}
 }
